372a18f7b18fa7d28fb4eed67cde1d93ade09f8c
KentinMartinato <kentin.martinato@etud.univ-jfc.fr>
2026-05-13 15:52:30 +0200

    ok

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,3 +9,17 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes)
   ]
 };
+export class AppComponent {
+  selectedChambre: any = null;
+
+  chambres = [
+    { id: 1, numero: '101', type: 'Individuelle', etage: 1, disponible: true, equipements: ['TV', 'Wifi', 'Climatisation'] },
+    { id: 2, numero: '102', type: 'Double', etage: 1, disponible: false, equipements: ['TV', 'Wifi'] },
+    { id: 3, numero: '201', type: 'Soins intensifs', etage: 2, disponible: true, equipements: ['Moniteur cardiaque', 'Oxygène'] },
+    { id: 4, numero: '205', type: 'Individuelle', etage: 2, disponible: true, equipements: ['TV', 'Balcon'] },
+  ];
+
+  selectChambre(chambre: any) {
+    this.selectedChambre = chambre;
+  }
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,37 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('ServicePatrimoine');
+
+export class App implements OnInit {
+
+  locations : any[] = [];
+
+  selectedLocation : any =null;
+
+  constructor(private readonly http : HttpClient){}
+
+  ngOnInit(){
+    this.http.get<any[]>('/location.json').subscribe({
+      next: (data) => {
+        this.locations = data;
+        console.log('Données FHIR chargées :', this.locations);
+      },
+      error: (err) => {
+        console.error('Erreur lors du chargement du JSON :', err);
+      }
+  });
+  }
+
+  selectLocation(loc: any){
+    this.selectedLocation = loc;
+  }
 }
